b046d731676b577caad88c9fed7f3afec709aa18
Sukumar755 <sukumar755@gmail.com>
2026-02-05 19:59:57 -0600

    updating the project

--- src/SwitchPractice.java
+++ src/SwitchPractice.java
@@ -9,25 +9,25 @@ public class SwitchPractice {
          int day = sc.nextInt();
 
          switch(day){
-             case(2):
+             case(7):
                  System.out.println("Sunday");
                  break;
              case(1):
                  System.out.println("Monday");
                  break;
-             case(3):
+             case(2):
                  System.out.println("Tuesday");
                  break;
-             case(4):
+             case(3):
                  System.out.println("Wednesday");
                  break;
-             case(5):
+             case(4):
                  System.out.println("Thursday");
                  break;
-             case(6):
+             case(5):
                  System.out.println("Friday");
                  break;
-             case(7):
+             case(6):
                  System.out.println("Saturday");
                  break;
              default:
